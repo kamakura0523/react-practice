c5f3c918af55eca569ddf4f79ee09e91ed38531a
kamakura0523 <suieikama0523@gmail.com>
2024-09-24 18:40:53 +0900

    マウスイベント

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -1,6 +1,10 @@
 import { useState,useRef } from "react";
 import TodoList from "./TodoList";
 import { v4 as uuidv4 } from "uuid"
+import { BrowserRouter as Router, Route, Routes,Link} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 function App() {
   const [todos, setTodos] = useState([]);
 
@@ -34,7 +38,31 @@ function App() {
       <input type="text" ref={todoNameRef}/>
       <button onClick={handleAddTodo}>タスクを追加</button>
       <button onClick={handleClear}>完了したタスクの削除</button>
-      <div>残りのタスク:{todos.filter((todo) => !todo.completed).length}</div>    
+      <div>残りのタスク:{todos.filter((todo) => !todo.completed).length}</div>
+      <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/Home">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* ページコンポーネントのルーティング */}
+        <Routes>
+          <Route path="/Home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>  
       </div>
   )
 }
--- my-react/src/pages/Home.jsx
+++ my-react/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 // Home.js
-import React from 'react';
+import React from "react";
 
 const Home = () => {
   return (
