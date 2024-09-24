1c412132792bde61d8f9e0489438c30bb84ed0f0
kamakura0523 <suieikama0523@gmail.com>
2024-09-24 18:37:05 +0900

    マウスのイベント作成

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
+import EventMouse from './EventMouse';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  
   <React.StrictMode>
     <App />
+    <EventMouse alt={"ロゴ画像"} beforeSrc="https://www.web-deli.com/image/linkbanner_l.gif" afterSrc="https://www.web-deli.com/image/home_chara.gif"
+  />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="Contact">
+      <h1>Contact Page</h1>
+      <p>This is the Contact component.</p>
+    </div>
+  );
+};
+
+export default Contact;
